02fc12f8996fdb561ffad8867c7076d32a9b0c05
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2007-06-13 13:38:44 +0000

    Ifdefed out an unnecessary change of group flags just before the channel is closed
    20070613133844-5b6ca-03f41cac66c322daf1e4e65ed29b07ed5e94745a.gz

--- src/sip-media-channel.c
+++ src/sip-media-channel.c
@@ -941,9 +941,11 @@ static void priv_session_state_changed_cb (SIPMediaSession *session,
     tp_group_mixin_change_members ((GObject *)channel,
         "", NULL, set, NULL, NULL, 0, 0);
 
+#if 0
     /* update flags accordingly -- allow adding, deny removal */
     tp_group_mixin_change_flags ((GObject *)channel,
         TP_CHANNEL_GROUP_FLAG_CAN_ADD, TP_CHANNEL_GROUP_FLAG_CAN_REMOVE);
+#endif
 
     priv_destroy_session (channel);
     sip_media_channel_close (channel);
