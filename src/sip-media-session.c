e76b92ee7ad5d9c2e6c3ead9e29fe9da15fc3135
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2007-07-06 17:53:35 +0000

    SIPMediaSession: don't destroy the call handle, only unref it; this softens the effect of some alleged Sofia-SIP problems
    20070706175335-5b6ca-8fea3e0a0898c429a1368269ff37d9cf9f663218.gz

--- src/sip-media-session.c
+++ src/sip-media-session.c
@@ -554,11 +554,11 @@ priv_session_state_changed (SIPMediaSession *session,
       break;
     case SIP_MEDIA_SESSION_STATE_ENDED:
       priv_close_all_streams (session);
-      DEBUG("destroying the NUA handle %p", priv->nua_op);
+      DEBUG("freeing the NUA handle %p", priv->nua_op);
       if (priv->nua_op != NULL)
         {
           nua_handle_bind (priv->nua_op, SIP_NH_EXPIRED);
-          nua_handle_destroy (priv->nua_op);
+          nua_handle_unref (priv->nua_op);
           priv->nua_op = NULL;
         }
       break;
