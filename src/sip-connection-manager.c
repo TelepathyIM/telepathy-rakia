dbe1eed608f5d43a58674af1acc6b1343b077207
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2008-02-11 16:05:01 +0000

    SIPConnectionManager: consistently nullify all parameter strings if empty
    20080211160501-5b6ca-625246c40f6e2a0fe60930b787da73d5f9d6256d.gz

--- src/sip-connection-manager.c
+++ src/sip-connection-manager.c
@@ -406,6 +406,8 @@ sip_connection_manager_new_connection (TpBaseConnectionManager *base,
   /* FIXME: validate account SIP URI properly, using appropriate RFCs */
   if (!check_not_empty_if_present ("account", params->account, error))
     return FALSE;
+  NULLIFY_IF_EMPTY(params->auth_user);
+  NULLIFY_IF_EMPTY(params->password);
   /* FIXME: validate registrar SIP URI properly, using appropriate RFCs */
   NULLIFY_IF_EMPTY(params->registrar);
   /* FIXME: validate proxy host properly */
@@ -420,6 +422,8 @@ sip_connection_manager_new_connection (TpBaseConnectionManager *base,
   NULLIFY_IF_EMPTY(params->stun_server);
   /* FIXME: validate local IP address properly */
   NULLIFY_IF_EMPTY(params->local_ip_address);
+  NULLIFY_IF_EMPTY(params->extra_auth_user);
+  NULLIFY_IF_EMPTY(params->extra_auth_password);
 
   DEBUG("New SIP connection to %s", params->account);
 
