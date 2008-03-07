7c2c903e76a3dce01d732849546dca766d6826bb
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2008-03-07 14:37:39 +0000

    Used the occasion to update copyright notices in the code:
    added missing notices, updated Nokia copyright years, removed "this library"
    language, humbly added myself to the author lists.
    
    20080307143739-5b6ca-0a4e19949140f3fb974d6b0bff79debf76cf9f9c.gz

--- src/debug.c
+++ src/debug.c
@@ -1,3 +1,22 @@
+/*
+ * debug.h - Debug helpers for Telepathy-SofiaSIP, implementation
+ * Copyright (C) 2007-2008 Nokia Corporation
+ *
+ * This work is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "config.h"
 
 #ifdef ENABLE_DEBUG
--- src/sip-connection-manager.c
+++ src/sip-connection-manager.c
@@ -1,24 +1,26 @@
 /*
  * sip-connection-manager.c - Source for TpsipConnectionManager
  * Copyright (C) 2005-2007 Collabora Ltd.
- * Copyright (C) 2005-2007 Nokia Corporation
+ * Copyright (C) 2005-2008 Nokia Corporation
  *   @author Kai Vehmanen <first.surname@nokia.com>
  *   @author Martti Mela <first.surname@nokia.com>
+ *   @author Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
  *
  * Based on telepathy-gabble implementation (gabble-connection-manager).
  *   @author See gabble-connection-manager.c
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
+ * This work is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This work is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
+ * License along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
