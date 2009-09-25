a0d04fac27d43aa388e3dfe76d16178c564089bc
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2009-09-25 20:38:03 +0300

    Test setting alias for the self handle
    It's expected to produce a properly quoted display name in the From header
    for outbound requests.

---
+++ tests/twisted/test-self-alias.py
@@ -0,0 +1,40 @@
+#
+# Test alias setting for the self handle
+#
+
+from sofiatest import exec_test
+from servicetest import tp_name_prefix
+
+import dbus
+
+TEXT_TYPE = tp_name_prefix + '.Channel.Type.Text'
+
+def test(q, bus, conn, sip_proxy):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
+
+    self_handle = conn.GetSelfHandle()
+
+    conn.Aliasing.SetAliases({self_handle: 'foo@bar.baz'})
+
+    event = q.expect('dbus-signal', signal='AliasesChanged',
+        args=[[(self_handle, u'foo@bar.baz')]])
+
+    handle = conn.RequestHandles(1, ['sip:user@somewhere.com'])[0]
+    conn.RequestChannel(TEXT_TYPE, 1, handle, True)
+
+    event = q.expect('dbus-signal', signal='NewChannel')
+
+    text_iface = dbus.Interface(bus.get_object(conn.bus_name, event.args[0]),
+                               TEXT_TYPE)
+    text_iface.Send(0, 'Hello')
+
+    event = q.expect('sip-message')
+
+    self_uri = conn.InspectHandles(1, [self_handle])[0]
+
+    from_header = event.sip_message.headers['from'][0]
+    assert from_header.startswith('"foo@bar.baz" <' + self_uri + '>'), from_header
+
+if __name__ == '__main__':
+    exec_test(test)
