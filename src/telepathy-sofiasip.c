c300e733d5d187f61bc36f2ffe2cbad46042b1b6
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2010-03-29 20:40:17 +0300

    Make TPSIP_PERSIST environment variable work regardless of ENABLE_DEBUG
    The --enable-debug configure option, and the ENABLE_DEBUG macro, should
    control debug logging and its possible overhead; there is no penalty
    in making the persistent mode work independently of the debug switch.

--- src/telepathy-sofiasip.c
+++ src/telepathy-sofiasip.c
@@ -107,12 +107,12 @@ main (int argc, char** argv)
 
 #ifdef ENABLE_DEBUG
   tpsip_debug_set_flags_from_env ();
+#endif
 
   if (g_getenv ("TPSIP_PERSIST") || g_getenv ("SOFIASIP_PERSIST"))
     {
       tp_debug_set_persistent (TRUE);
     }
-#endif
 
   tp_debug_divert_messages (g_getenv ("TPSIP_LOGFILE"));
 
