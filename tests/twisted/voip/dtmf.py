a3ff78f02af75261d5b322610a679a191082e927
Mikhail Zabaluev <mikhail.zabaluev@nokia.com>
2011-08-24 15:36:03 +0300

    Test that initial tones are signalled as playing upon call establishment

--- tests/twisted/voip/dtmf.py
+++ tests/twisted/voip/dtmf.py
@@ -7,12 +7,14 @@ from servicetest import (
     wrap_channel,
     assertEquals, assertContains, assertLength, assertSameSets
     )
-
+from voip_test import VoipTestContext
 import constants as cs
 
 def request_initial_tones(q, bus, conn, sip_proxy, peer='foo@bar.com'):
     conn.Connect()
     q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
+
+    context = VoipTestContext(q, conn, bus, sip_proxy, 'sip:testacc@127.0.0.1', peer)
 
     path = conn.Requests.CreateChannel({
             cs.CHANNEL_TYPE: cs.CHANNEL_TYPE_STREAMED_MEDIA,
@@ -23,7 +25,7 @@ def request_initial_tones(q, bus, conn, sip_proxy, peer='foo@bar.com'):
         })[0]
 
     chan = wrap_channel(bus.get_object(conn.bus_name, path), 'StreamedMedia',
-        ['DTMF'])
+        ['MediaSignalling', 'DTMF'])
 
     channel_props = chan.Properties.GetAll(cs.CHANNEL)
 
@@ -32,6 +34,21 @@ def request_initial_tones(q, bus, conn, sip_proxy, peer='foo@bar.com'):
     dtmf_props = chan.Properties.GetAll(cs.CHANNEL_IFACE_DTMF)
 
     assertEquals('1234', dtmf_props['InitialTones'])
+    assertEquals(False, dtmf_props['CurrentlySendingTones'])
+
+    stream_handler = context.handle_audio_session(chan)
+
+    invite_event = q.expect('sip-invite')
+
+    context.accept(invite_event.sip_message)
+
+    stream_handler.StreamState(cs.MEDIA_STREAM_STATE_CONNECTED)
+
+    q.expect('dbus-signal', signal='SendingTones', args=['1234'])
+
+    dtmf_props = chan.Properties.GetAll(cs.CHANNEL_IFACE_DTMF)
+
+    assertEquals(True, dtmf_props['CurrentlySendingTones'])
 
 if __name__ == '__main__':
     exec_test(request_initial_tones)
