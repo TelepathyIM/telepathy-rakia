46e0f63e59df3cbf2a8c5f49b32b48b233f45e19
Simon McVittie <simon.mcvittie@collabora.co.uk>
2012-05-09 19:18:33 +0100

    Consistently include config.h in every .c file, first
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=49725
    Signed-off-by: Simon McVittie <simon.mcvittie@collabora.co.uk>
    Reviewed-by: Mikhail Zabaluev <mikhail.zabaluev@nokia.com>

--- rakia/codec-param-formats.c
+++ rakia/codec-param-formats.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include "config.h"
+
 #include "codec-param-formats.h"
 
 #include <string.h>
--- tests/tp_caller.c
+++ tests/tp_caller.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include "config.h"
+
 #ifndef DBUS_API_SUBJECT_TO_CHANGE
 #define DBUS_API_SUBJECT_TO_CHANGE
 #endif
